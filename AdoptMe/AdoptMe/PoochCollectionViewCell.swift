6d95a420094c0163691499963cb3fd4f1f51b06c
san2ride <sanchezjason@me.com>
2016-07-08 11:20:12 -0600

    Round 9 - CollectionView created/DataStore implemented

---
+++ AdoptMe/AdoptMe/PoochCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PoochCollectionViewCell.swift
+//  AdoptMe
+//
+//  Created by don't touch me on 7/8/16.
+//  Copyright © 2016 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+
+class PoochCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionView: UIImageView!
+    
+    
+    
+}
